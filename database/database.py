b3681824dac4212b0774982c049cca02a7a0c638
sarveshg284-gif <sarveshghadi284@gmail.com>
2026-07-15 11:52:14 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,81 +1,114 @@
 import sqlite3
-import pandas as pd
 
-conn = sqlite3.connect("database/transaction.db", check_same_thread=False)
+conn = sqlite3.connect(
+    "transactions.db",
+    check_same_thread=False
+)
+
 cursor = conn.cursor()
 
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT UNIQUE,
+password TEXT,
+role TEXT
+)
+""")
 
-# ---------------- TRANSACTIONS ----------------
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS transactions(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    category TEXT,
-    quantity INTEGER,
-    from_location TEXT,
-    to_client TEXT,
-    employee TEXT,
-    date TEXT,
-    status TEXT,
-    remarks TEXT
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+item TEXT,
+quantity INTEGER,
+from_location TEXT,
+client TEXT,
+employee TEXT,
+date TEXT,
+remarks TEXT
 )
 """)
+
+
+cursor.execute("""
+INSERT OR IGNORE INTO users
+(username,password,role)
+VALUES
+('admin','admin123','Admin'),
+('staff','staff123','Staff')
+""")
+
 
 conn.commit()
 
 
-# ================= TRANSACTIONS =================
 
-def add_transaction(
-    category,
-    quantity,
-    from_location,
-    to_client,
-    employee,
-    date,
-    status,
-    remarks,
-):
+def login(username,password):
+
     cursor.execute(
         """
-        INSERT INTO transactions(
-            category,
-            quantity,
-            from_location,
-            to_client,
-            employee,
-            date,
-            status,
-            remarks
+        SELECT * FROM users
+        WHERE username=? AND password=?
+        """,
+        (username,password)
+    )
+
+    return cursor.fetchone()
+
+
+
+def add_transaction(
+    item,
+    quantity,
+    from_location,
+    client,
+    employee,
+    date,
+    remarks
+):
+
+    cursor.execute(
+        """
+        INSERT INTO transactions
+        VALUES(
+        NULL,?,?,?,?,?,?,?
         )
-        VALUES(?,?,?,?,?,?,?,?,?,?)
         """,
         (
-            category,
+            item,
             quantity,
             from_location,
-            to_client,
+            client,
             employee,
             date,
-            status,
-            remarks,
-        ),
+            remarks
+        )
     )
 
     conn.commit()
 
 
+
 def get_transactions():
-    return pd.read_sql(
-        "SELECT * FROM transactions ORDER BY id DESC",
-        conn,
+
+    cursor.execute(
+        """
+        SELECT * FROM transactions
+        ORDER BY id DESC
+        """
     )
 
+    return cursor.fetchall()
 
-def delete_transaction(transaction_id):
+
+
+def delete_transaction(id):
+
     cursor.execute(
         "DELETE FROM transactions WHERE id=?",
-        (transaction_id,),
+        (id,)
     )
+
     conn.commit()
