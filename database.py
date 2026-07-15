d1717ad1a17fd055874015282cc72a8c74263805
sarveshg284-gif <sarveshghadi284@gmail.com>
2026-07-15 12:44:00 +0530

    Rename transaction functions for consistency

--- database.py
+++ database.py
@@ -28,7 +28,7 @@ conn.commit()
 
 
 
-def add_transactions(
+def add_transaction(
     item,
     quantity,
     from_location,
@@ -81,7 +81,7 @@ def get_transactions():
 
 
 
-def delete_transactions(transaction_id):
+def delete_transaction(transaction_id):
 
     cursor.execute(
         """
