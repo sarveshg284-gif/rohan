cc2f5f7de8e3411187c0ce20d84ee64baae014d6
sarveshg284-gif <sarveshghadi284@gmail.com>
2026-07-15 11:23:39 +0530

    Update add_transactions.py

--- pages/add_transactions.py
+++ pages/add_transactions.py
@@ -1,52 +1,103 @@
 import streamlit as st
 from datetime import date
+from database.database import add_transaction
+
 
 st.title("➕ Add Transaction")
 
+
 with st.form("transaction"):
 
-    ###description = st.text_input("Description")
 
     category = st.selectbox(
         "Category",
-        ["Laptop","Keyboard","CPU","Mouse","Other"]
+        [
+            "Laptop",
+            "Keyboard",
+            "CPU",
+            "Mouse",
+            "Other"
+        ]
     )
+
 
     quantity = st.number_input(
         "Quantity",
         min_value=1
     )
 
-    from_location = st.text_input("From Location")
 
-    client = st.text_input("Client Site")
+    from_location = st.text_input(
+        "From Location"
+    )
 
-    employee = st.text_input("Client Name")
 
-    ###vehicle = st.text_input("Vehicle Number")
+    client = st.text_input(
+        "Client Site"
+    )
+
+
+    employee = st.text_input(
+        "Client Name"
+    )
+
 
     transaction_date = st.date_input(
-    "Transaction Date",
-    value=date.today()
+        "Transaction Date",
+        value=date.today()
     )
 
 
     status = st.selectbox(
         "Status",
-        ["Pending","In Transit","Delivered"]
+        [
+            "Pending",
+            "In Transit",
+            "Delivered"
+        ]
     )
 
-    status = st.selectbox(
-    "Remark",
-    ["✔️", "❌"]
+
+    remark = st.selectbox(
+        "Remark",
+        [
+            "✔️",
+            "❌"
+        ]
     )
-        
+
+
     upload = st.file_uploader(
         "Upload Document",
-        ["pdf","jpg","png"]
+        type=[
+            "pdf",
+            "jpg",
+            "png"
+        ]
     )
 
-    submit = st.form_submit_button("Save")
+
+    submit = st.form_submit_button(
+        "Save"
+    )
+
 
 if submit:
-    st.success("Transaction Saved Successfully")
+
+    add_transaction(
+        category,              # description
+        category,
+        quantity,
+        from_location,
+        client,
+        employee,
+        "",                    # vehicle
+        str(transaction_date),
+        status,
+        remark
+    )
+
+
+    st.success(
+        "Transaction Saved Successfully"
+    )
