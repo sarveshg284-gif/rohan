c41b0bea64d3b9e70342ffacd2b386debd36826a
sarveshg284-gif <sarveshghadi284@gmail.com>
2026-07-15 11:55:16 +0530

    Enhance reports with data visualization and download options

--- pages/reports.py
+++ pages/reports.py
@@ -1,15 +1,56 @@
 import streamlit as st
+import pandas as pd
+from database import get_transactions
+
 
 st.title("📈 Reports")
 
-st.download_button(
-    "Download Excel",
-    data="Sample",
-    file_name="report.xlsx"
+
+df=pd.DataFrame(
+get_transactions(),
+columns=[
+"ID",
+"Item",
+"Quantity",
+"From",
+"Client",
+"Employee",
+"Date",
+"Remarks"
+]
 )
 
-st.download_button(
-    "Download CSV",
-    data="Sample",
-    file_name="report.csv"
+
+st.subheader(
+"Client Wise Report"
+)
+
+
+report=df.groupby(
+"Client"
+)["Quantity"].sum()
+
+
+st.bar_chart(
+report
+)
+
+
+st.subheader(
+"Monthly Report"
+)
+
+
+df["Date"]=pd.to_datetime(
+df.Date
+)
+
+
+monthly=df.groupby(
+df.Date.dt.month
+)["Quantity"].sum()
+
+
+st.line_chart(
+monthly
 )
